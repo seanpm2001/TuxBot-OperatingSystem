1602137c271569c295faf1dbc8afe55f4e9e8472
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-03 21:23:11 -0700

    REMOTE.c
    The remote control script for TuxBot.

---
+++ Robot/Remote/REMOTE.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// The remote for TuxBot
+// I don't know how I originally overlooked the controls, but I had to come up with a decision on controls. There will be an automatic mode, and a remote controlled mode. This script is for the remote-controlled mode
+// I am not very good at programming in C, I should have gone with C++ or something else to get some features, but I chose C for the hardware functionality. This is not yet functional, and there is lots of room for improvement.
+// Definitions
+union remoteUnion (1) // Trying to form a class here, so it can detect from different Fedora versions.
+// Main
+int main(void) {
+    return remoteControlMode();
+    break;
+    exit;
+} // End of main method
+int remoteControlMode {
+    printf("Remote for Tux\nVersion 0.001");
+    controlGrid.method("[<-] [^] ]->] [v]");
+    scanf(char:input);
+    break;
+}
+int speakButton {
+    printf("Speak for Tux\nVersion 0.001");
+    scanf("What would you like Tux to say?\n[Linux fact] (A)\n[Unknown] (B)");
+    if (input1 = "A") {
+      scanf("/Say/Linux/Facts/LINUX_FACTS.txt");
+      file return(random:char(5, 10));
+      break;
+    }
+    else {
+      return speakButton();
+      break;
+    }
+  break;
+}
+int controlGrid {
+ // Coming soon
+  break;
+}
+int moveNorth {
+  // Coming soon
+  break;
+}
+int moveEast {
+  // Coming soon
+  break;
+}
+int moveSouth {
+  // Coming soon
+  break;
+}
+int moveWest {
+  // Coming soon
+  break;
+}
+int jump {
+  // Coming soon
+  break;
+}
+int waddleToggle { // Toggles waddling on and off
+  // Coming soon
+  break;
+}
+/* File info
+* File version: `1 (Thursday, June 3rd 2021 at 9:23 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 70
+*/
+// This script is incomplete, and needs lots of work
+// End of script
