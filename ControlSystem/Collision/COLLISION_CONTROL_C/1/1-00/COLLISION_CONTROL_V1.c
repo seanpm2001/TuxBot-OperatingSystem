caa0a4d568dd800ce5bc784a2f4450fb7c1d87a1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-08 20:07:16 -0700

    COLLISION_CONTROL_V1.c
    An archive of the first version of the collision control script for TuxBot.

---
+++ ControlSystem/Collision/COLLISION_CONTROL_C/1/1-00/COLLISION_CONTROL_V1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// Collision control scripting for TuxBot.
+// I am not very good at programming in C, I should have gone with C++ or something else to get some features, but I chose C for the hardware functionality. This is not yet functional, and there is lots of room for improvement.
+// Definitions
+union collisionControlUnion (1)
+// Main
+int main(void) {
+    return nil();
+    break;
+    exit;
+} // End of main method
+int bumped {
+  bool bump = false;
+  if touching {} {
+    bump = true;
+  }
+  if bump == true {
+   return fullStop();
+   log "bump";
+   break;
+  }
+  break;
+}
+int detectSolid {
+  // Method currently undefined 
+  // This method is for stopping Tux from continuing forward if there is a detected object. I want to be able to let Tux push stuff, so a separate push option is included
+  break;
+}
+int pushShove {
+  // Method currently undefined 
+  // This method is for stopping Tux from continuing forward if there is a detected object, unless the user reports they want to push or shove this item, at which Tux will slow down and start pushing or shoving the object forward
+  break;
+}
+int fullStop {
+ // Method currently undefined 
+  break;
+}
+/* File info
+* File version: `1 (Tuesday, June 8th 2021 at 8:06 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 47
+*/
+// This script is incomplete, and needs lots of work
+// End of script
